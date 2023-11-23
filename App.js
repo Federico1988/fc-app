99a340f1a45cdc5c9bc5e5d4e26c54651b802f98
Fede C <federico.cipolletta@diestroweb.com>
2023-11-22 22:38:57 -0300

    Clase coreComponents

--- App.js
+++ App.js
@@ -1,17 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button, Alert } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Clickear el boton para ver "Hola Coder"</Text>
-      <StatusBar style="auto" />
-      <Button
-        onPress={() => Alert.alert('Hola Coder!')}
-        title="Click!"
-        color="#841584"
-        accessibilityLabel="Learn more about this purple button"
-      />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} />
+        <Button title="ADD" />
+      </View>
+      <View style={styles.listContainer}>
+        <View style={styles.cardProduct}>
+          <Text style={styles.cardText}>Producto 1</Text>
+          <Text>$432</Text>
+          <Button title='DEL' />
+        </View>
+
+        <View style={styles.cardProduct}>
+          <Text style={styles.cardText}>Producto 2</Text>
+          <Text>$432</Text>
+          <Button title='DEL' />
+        </View>
+        <View style={styles.cardProduct}>
+          <Text style={styles.cardText}>Producto 3</Text>
+          <Text>$432</Text>
+          <Button title='DEL' />
+        </View>
+      </View>
     </View>
   );
 }
@@ -21,7 +35,32 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-    gap: 40
+    justifyContent: 'start',
+    marginTop: 62
   },
+  inputContainer: {
+    flexDirection: "row",
+    alignSelf: "strech",
+    justifyContent: "space-around"
+  },
+  input: {
+    borderWidth: 4,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    width: 200
+  },
+  listContainer: {
+    padding: 5,
+    width: "100%"
+  },
+  cardProduct: {
+    marginTop:20,
+    borderWidth: 4,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center"
+  },
+  cardText: {
+  }
 });
+
